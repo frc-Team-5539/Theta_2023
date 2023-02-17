dafbe6f97897537877056edb69954762ca31608f
DVHS 5539 CYBORGS <61916427+DVHSCYBORGS5539@users.noreply.github.com>
2023-02-17 14:08:32 -0700

    Add files via upload

---
+++ Robot.cpp
@@ -0,0 +1,260 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+#include <frc/Timer.h>
+#include <fmt/core.h>
+
+#include <frc/smartdashboard/SmartDashboard.h>
+
+void Robot::RobotInit() {
+  //m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  //m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  //frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+      #if defined(__linux__) || defined(_WIN32)
+  frc::CameraServer::StartAutomaticCapture();
+  #else
+    std::fputs("Vision only available on Linux or Windows.\n", stderr);
+    std::fflush(stderr);
+#endif
+  //frc::CameraServer::SetSize	(	kSize640x480 );
+ frc::CameraServer::PutVideo	(	DriveCam, 1280, 720);
+
+ //Fix Camera
+    
+     // The RestoreFactoryDefaults method can be used to reset the configuration parameters
+     // in the SPARK MAX to their factory default state. If no argument is passed, these
+     // parameters will not persist between power cycles
+     //
+  //m_leftLeadMotor.RestoreFactoryDefaults();
+  //m_rightLeadMotor.RestoreFactoryDefaults();
+  //m_leftFollowMotor.RestoreFactoryDefaults();
+  //m_rightFollowMotor.RestoreFactoryDefaults();
+
+  m_robotDrive.DriveCartesian (-m_driverController.GetLeftY(), -m_driverController.GetLeftX(),
+                                -m_driverController.GetRightX());
+  m_frontLeft.SetInverted(true);
+  m_rearLeft.SetInverted(true);
+  m_robotDrive.SetExpiration(100_ms);
+  m_timer.Start();
+}
+
+/**
+ * This function is called every 20 ms, no matter the mode. Use
+ * this for items like diagnostics that you want ran during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic()  {
+ 
+  frc::SmartDashboard::PutNumber("ia: ", ia);
+  frc::SmartDashboard::PutNumber("it: ", it);
+
+  frc::SmartDashboard::PutBoolean("LB : ", leftbumper);
+  frc::SmartDashboard::PutBoolean("RB : ", rightbumper);
+
+  frc::SmartDashboard::PutBoolean("LT : ", lefttriggeraxis);
+  frc::SmartDashboard::PutBoolean("RT : ", righttriggeraxis);
+
+  frc::SmartDashboard::PutBoolean("bA : ", bA);
+  frc::SmartDashboard::PutBoolean("bB : ", bB);
+  frc::SmartDashboard::PutBoolean("bX : ", bX);
+  frc::SmartDashboard::PutBoolean("bY : ", bY);
+
+  frc::SmartDashboard::PutNumber("left x : ", left_x);
+  frc::SmartDashboard::PutNumber("left y : ", left_y);
+  frc::SmartDashboard::PutNumber("right x : ", right_x);
+  frc::SmartDashboard::PutNumber("right y : ", right_y); 
+
+  frc::SmartDashboard::PutNumber("ai raw :", ai_raw);
+  frc::SmartDashboard::PutNumber("ai voltage :", ai_voltage);
+
+  frc::SmartDashboard::PutNumber("ultra raw :", ultra_raw);
+  frc::SmartDashboard::PutNumber("ultra distance :", distance);
+
+}
+
+/**
+ * This autonomous (along with the chooser code above) shows how to select
+ * between different autonomous modes using the dashboard. The sendable chooser
+ * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+ * remove all of the chooser code and uncomment the GetString line to get the
+ * auto name from the text box below the Gyro.
+ *
+ * You can add additional auto modes by adding additional comparisons to the
+ * if-else structure below with additional strings. If using the SendableChooser
+ * make sure to add them to the chooser code above as well.
+ */
+
+
+void Robot::AutonomousInit() {
+  //m_autoSelected = m_chooser.GetSelected();
+  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
+  //     kAutoNameDefault);
+  //fmt::print("Auto selected: {}\n", m_autoSelected);
+
+  //if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+      m_timer.Reset();
+    m_timer.Start();
+    ia = 0;
+
+} 
+  //else {
+    // Default Auto goes here
+  
+
+
+void Robot::AutonomousPeriodic() 
+  //if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+   {
+     if (m_timer.Get() < 2_s) {
+      // Drive forwards quarter speed, make sure to turn input squaring off
+      m_robotDrive.DriveCartesian(-0.25, 0.0, false);
+    } else {
+      // Stop robot
+      m_robotDrive.DriveCartesian(0.0, 0.0, false);
+  }
+  ia++;
+}
+
+
+void Robot::TeleopInit() {
+    it = 0;
+}
+
+void Robot::TeleopPeriodic() {
+  it++;
+
+  left_y = m_driverController.frc::XboxController::GetLeftX(),
+  right_y = m_driverController.frc::XboxController::GetRightX();
+
+  left_x = m_driverController.frc::XboxController::GetLeftY(),
+  right_x = m_driverController.frc::XboxController::GetRightY();
+
+  leftbumper = m_driverController.frc::XboxController::GetLeftBumper();
+  rightbumper = m_driverController.frc::XboxController::GetRightBumper();
+
+  lefttriggeraxis = m_driverController.frc::XboxController::GetLeftTriggerAxis();
+  righttriggeraxis = m_driverController.frc::XboxController::GetRightTriggerAxis();
+
+  bA = m_driverController.GetAButton();
+  bB = m_driverController.GetBButton();
+  bX = m_driverController.GetXButton();
+  bY = m_driverController.GetYButton();
+
+  //limit_switch_value = limit_switch.Get();
+  //knife_switch_value = knife_switch.Get();
+
+  ai_raw = ai.GetValue();
+  ai_voltage = ai.GetVoltage();
+
+  ultra_raw = ultra.GetValue();
+  distance = (double) ultra_raw * adc_to_mm ;
+
+//Drive Cartesian
+  m_robotDrive.DriveCartesian(-m_driverController.GetLeftX(), - -m_driverController.GetLeftY(),
+                               - -m_driverController.GetRightTriggerAxis() + -m_driverController.GetLeftTriggerAxis());
+
+// -m_driverController.GetRightX()
+//When we calibrated XBox controller, Z-axis showed up on triggers so switched back to triggers 
+//DID IT-CARLOS!
+
+
+//armextender lengthen using PWM Sparks paired
+m_armextender.Set(0.05);
+m_armextender.Set(leftbumper);
+//armextender shorten
+m_armextender.Set(-0.05);
+m_armextender.Set(rightbumper);
+
+//Pneumatics
+//Arm up and down
+m_updownarm.Set(frc::DoubleSolenoid::Value::kOff);
+m_updownarm.Set(frc::DoubleSolenoid::Value::kForward);
+m_updownarm.Set(frc::DoubleSolenoid::Value::kReverse);
+
+// Initialize the DoubleSolenoid so it knows where to start.  Not required for single solenoids.
+m_updownarm.Set(frc::DoubleSolenoid::kForward);
+if (m_driverController.GetXButtonPressed()) {
+   m_updownarm.Toggle();
+   m_updownarm.Toggle();
+}
+
+
+//Grabber
+// Initialize the DoubleSolenoid so it knows where to start.  Not required for single solenoids.
+
+m_grabber.Set(frc::DoubleSolenoid::Value::kOff);
+m_grabber.Set(frc::DoubleSolenoid::Value::kForward);
+m_grabber.Set(frc::DoubleSolenoid::Value::kReverse);
+
+m_grabber.Set(frc::DoubleSolenoid::Value::kReverse);
+
+if (m_driverController.GetBButtonPressed()) {
+   m_grabber.Toggle();
+   m_grabber.Toggle();
+}
+
+  //Claw
+  //double rightTrigger = m_joystick.GetTriggerAxis(frc::GenericHID::kRightHand);
+  // bool leftTrigger = m_joystick.GetTriggerAxis(frc::GenericHID::kLeftHand);
+  //if (rightTrigger > 0.2)
+  //{
+  //m_claw.SetAngle(90);
+    //}
+    //if (leftTrigger > 0.2)
+   // {
+     // m_claw.SetAngle(0);
+   // }
+
+    //Claw Up
+    //bool yButton = kjoystick.GetYButton();
+    //bool aButton = kjoystick.GetAButton();
+    //if (yButton == true)
+    //{
+      //m_clawUp.Set(DoubleSolenoid::Value::kForward);
+    //}
+    //if (aButton == true)
+   // {
+      //m_clawUp.Set(DoubleSolenoid::Value::kReverse);
+    //}
+
+    //Claw Forward
+    //bool bButton = m_joystick.GetBButton();
+    //bool xButton = m_joystick.GetXButton();
+
+    //if (bButton == true)
+    //{
+      //m_clawExtend.Set(DoubleSolenoid::Value::kForward);
+    //}
+    //if (xButton == true)
+    //{
+     // m_clawExtend.Set(DoubleSolenoid::Value::kReverse);
+    //}                               
+}
+ 
+
+
+void Robot::DisabledInit() {}
+
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+
+void Robot::TestPeriodic() {}
+
+void Robot::SimulationInit() {}
+
+void Robot::SimulationPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
---
+++ Robot.h
@@ -0,0 +1,146 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/XboxController.h>
+#include <frc/GenericHID.h>
+#include <frc/DigitalInput.h>
+#include <frc/AnalogInput.h>
+#include <frc/Servo.h>
+#include <frc/motorcontrol/Spark.h>
+#include <frc/motorcontrol/MotorControllerGroup.h>
+#include <frc/motorcontrol/PWMMotorController.h>
+
+#include <cstdio>
+#include <cameraserver/CameraServer.h>
+
+
+//add these for SparkMax CAN 2 pairs of CIM motors (brushed)
+#include <frc/drive/MecanumDrive.h>
+#include <rev/CANSparkMax.h>
+#include <frc/kinematics/MecanumDriveWheelSpeeds.h>
+#include <frc/motorcontrol/PWMSparkMax.h>
+
+//Pneumatics
+#include <frc/Compressor.h>
+#include <frc/Solenoid.h>
+#include <frc/DoubleSolenoid.h>
+
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+  void SimulationInit() override;
+  void SimulationPeriodic() override;
+
+//drive 2 pairs of brushed CIM motors using CAN & SparkMax motor controller
+//static const int leftLeadDeviceID = 5, leftFollowDeviceID = 6, rightLeadDeviceID = 7, rightFollowDeviceID = 8;
+//rev::CANSparkMax m_leftLeadMotor{leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+//rev::CANSparkMax m_rightLeadMotor{rightLeadDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+//rev::CANSparkMax m_leftFollowMotor{leftFollowDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+//rev::CANSparkMax m_rightFollowMotor{rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  //const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  private:
+  static constexpr int kFrontLeftChannel = 8;
+  static constexpr int kRearLeftChannel = 7;
+  static constexpr int kFrontRightChannel = 5;
+  static constexpr int kRearRightChannel = 6;
+//CAN
+  rev::CANSparkMax m_frontLeft{kFrontLeftChannel, rev::CANSparkMax::MotorType::kBrushed};
+  rev::CANSparkMax m_rearLeft{kRearLeftChannel, rev::CANSparkMax::MotorType::kBrushed};
+  rev::CANSparkMax m_frontRight{kFrontRightChannel, rev::CANSparkMax::MotorType::kBrushed};
+  rev::CANSparkMax m_rearRight{kRearRightChannel, rev::CANSparkMax::MotorType::kBrushed};
+
+  frc::MecanumDrive m_robotDrive{m_frontLeft, m_rearLeft, m_frontRight,
+                                 m_rearRight};
+
+
+  int it = 0;
+  int ia = 0;
+
+  frc::XboxController m_driverController{0};
+  //frc::DigitalInput   limit_switch{9};
+  frc::AnalogInput    ai{0};
+  frc::AnalogInput    ultra{1};
+  
+  
+  frc::Timer         m_timer;
+
+  std::string_view   DriveCam;
+
+  //Pneumatics
+  //pneumatics cylindar to raise and lower arm
+  frc::DoubleSolenoid m_updownarm{1, frc::PneumaticsModuleType::REVPH, 0, 1};
+  
+  //Pneumatics cylindar to open and close grabber
+  frc::DoubleSolenoid  m_grabber{1, frc::PneumaticsModuleType::REVPH, 2, 3};
+
+
+
+  frc::Compressor phCompressor{1, frc::PneumaticsModuleType::REVPH};
+
+  //PWM
+  frc::Spark          m_left_motor_armextender{9};
+  frc::Spark          m_right_motor_armextender{8};
+
+  frc::MotorControllerGroup m_armextender{m_left_motor_armextender,  m_right_motor_armextender};
+
+
+  bool leftbumper = false;
+  bool rightbumper = false;
+
+  double lefttriggeraxis = 0.0;
+  double righttriggeraxis = 0.0;
+
+  bool bA = false;
+  bool bB = false;
+  bool bX = false;
+  bool bY = false;
+  double left_x = 0.0;
+  double left_y = 0.0;
+  double right_x = 0.0;
+  double right_y = 0.0;
+
+  //bool limit_switch_value = false;
+  //bool knife_switch_value = false;
+
+  int ai_raw  = 0;
+  double ai_voltage = 0.0;
+
+  int ultra_raw = 0;
+  double distance = 0.0;
+  double adc_to_mm = 1.25 / 1000.0 ;
+
+  double xSpeed = 0.0;
+  double ySpeed = 0.0;
+  double zRotation = 0.0;
+
+  double kOff = 0.0;
+  double kReverse = 0.0;
+  double kForward = 0.0;
+
+  int kSize640x480 = 0;
+};
+
+
